3e68b3c6a7a701c4eb1bb7bf218b4b17e5fbce29
marsgrateful <45170630+marsgrateful@users.noreply.github.com>
2018-11-28 22:03:45 +0800

    new url sheet
    new url sheet

--- gs-linebot.js
+++ gs-linebot.js
@@ -6,7 +6,7 @@ function doGet(e){
     return;
   }
   // 記得sheet必須要開放權限 need open limit of authority
-var sheet =  SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1KbXKifdT0eJnjHoacke7dl49x-Ka33HXYubfXc3e3pE/edit#gid=0');
+var sheet =  SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1vuHzgakq1qADJgK5qioWQefoqcX5yWYjAB4TnqAM13g/edit?usp=sharing');
   sheet.appendRow(["start",temperature]);
   
 
